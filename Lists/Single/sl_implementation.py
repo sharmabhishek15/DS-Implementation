04c1dc157df2ef836cab0e6aa3ac35248c1c90ed
sharmabhishek15 <sharmabhishek15@outlook.com>
2021-01-17 18:46:34 +0530

    delete prg

--- Lists/Single/sl_implementation.py
+++ Lists/Single/sl_implementation.py
@@ -13,8 +13,8 @@ class LinkedList:
 	def insertAtFirst(self,new_data):
 		if self.head == None:
 			new_node = Node(new_data)
-			self.new_node = self.head
-			self.head = new_node
+		self.new_node = self.head
+		self.head = new_node
 
 
 	def insertAfter(self,prev_node,new_data):
@@ -26,89 +26,88 @@ class LinkedList:
 		prev_node.next = new_node
 
 
-    def append(self, new_data): 
-  
-        # 1. Create a new node 
-        # 2. Put in the data 
-        # 3. Set next as None 
-        new_node = Node(new_data) 
-  
-        # 4. If the Linked List is empty, then make the 
-        #    new node as head 
-        if self.head is None: 
-            self.head = new_node 
-            return
-  
-        # 5. Else traverse till the last node 
-        last = self.head 
-        while (last.next): 
-            last = last.next
-  
-        # 6. Change the next of last node 
-        last.next =  new_node
+	def append(self, new_data):   
+		# 1. Create a new node 
+		# 2. Put in the data 
+		# 3. Set next as None 
+
+		new_node = Node(new_data)
+		# 4. If the Linked List is empty, then make the 
+		#    new node as head
+		if self.head is None: 
+			self.head = new_node 
+			return
+
+		# 5. Else traverse till the last node 
+		last = self.head 
+		while (last.next): 
+			last = last.next
+
+		# 6. Change the next of last node 
+		last.next =  new_node
 
 
-    # Given a reference to the head of a list and a key,  
-    # delete the first occurence of key in linked list  
-    def deleteNode(self, key):  
-          
-        # Store head node  
-        temp = self.head  
+	# Given a reference to the head of a list and a key,  
+	# delete the first occurence of key in linked list  
+	def deleteNode(self, key):  
+		  
+		# Store head node  
+		temp = self.head  
   
-        # If head node itself holds the key to be deleted  
-        if (temp is not None):  
-            if (temp.data == key):  
-                self.head = temp.next
-                temp = None
-                return
+		# If head node itself holds the key to be deleted  
+		if (temp is not None):  
+			if (temp.data == key):  
+				self.head = temp.next
+				temp = None
+				return
   
-        # Search for the key to be deleted, keep track of the  
-        # previous node as we need to change 'prev.next'  
-        while(temp is not None):  
-            if temp.data == key:  
-                break
-            prev = temp  
-            temp = temp.next
+		# Search for the key to be deleted, keep track of the  
+		# previous node as we need to change 'prev.next'  
+		while(temp is not None):  
+			if temp.data == key:  
+				break
+			prev = temp  
+			temp = temp.next
   
-        # if key was not present in linked list  
-        if(temp == None):  
-            return
+		# if key was not present in linked list  
+		if(temp == None):  
+			return
   
-        # Unlink the node from linked list  
-        prev.next = temp.next
+		# Unlink the node from linked list  
+		prev.next = temp.next
   
-        temp = None
+		temp = None
 
 
-        # Utility function to print the linked list 
-    def printList(self): 
-        temp = self.head 
-        while (temp): 
-            print temp.data, 
-            temp = temp.next
+		# Utility function to print the linked list 
+	def printList(self): 
+		temp = self.head 
+		while (temp): 
+			print(temp.data) 
+			temp = temp.next
   
   
   
 # Code execution starts here 
 if __name__=='__main__': 
   
-    # Start with the empty list 
-    llist = LinkedList() 
+	# Start with the empty list 
+	llist = LinkedList() 
   
-    # Insert 6.  So linked list becomes 6->None 
-    llist.append(6) 
+	# Insert 6.  So linked list becomes 6->None 
+	llist.append(6) 
   
-    # Insert 7 at the beginning. So linked list becomes 7->6->None 
-    llist.push(7); 
+	# Insert 7 at the beginning. So linked list becomes 7->6->None 
+	llist.insertAtFirst(7)
   
-    # Insert 1 at the beginning. So linked list becomes 1->7->6->None 
-    llist.push(1); 
+	# Insert 1 at the beginning. So linked list becomes 1->7->6->None 
+	llist.insertAtFirst(1)
   
-    # Insert 4 at the end. So linked list becomes 1->7->6->4->None 
-    llist.append(4) 
+	# Insert 4 at the end. So linked list becomes 1->7->6->4->None 
+	llist.append(4) 
   
-    # Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None 
-    llist.insertAfter(llist.head.next, 8) 
+	# Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None 
+	llist.insertAfter(llist.head.next, 8) 
   
-    print('Created linked list is:')
-    llist.printList() 
+	print('Created linked list is:')
+	llist.printList() 
