32a3f6c4feda770ce4c4ba5f8c22cfc8a66c7e86
Abhishek <sharmabhishek15@outlook.com>
2024-11-05 21:28:30 +0530

    delete, reverse and middleNode functions added.

--- Lists/Single/sl_implementation.py
+++ Lists/Single/sl_implementation.py
@@ -1,5 +1,5 @@
 class Node:
-	def __init__(self,data):
+	def __init__(self, data):
 		self.data = data
 		self.next = None
 
@@ -10,14 +10,16 @@ class LinkedList:
 		self.head = None
 
 
-	def insertAtFirst(self,new_data):
+	def insertAtFirst(self, new_data):
 		if self.head == None:
 			new_node = Node(new_data)
-		self.new_node = self.head
+		else:
+			new_node = Node(new_data)
+			new_node.next = self.head
 		self.head = new_node
 
 
-	def insertAfter(self,prev_node,new_data):
+	def insertAfter(self, prev_node, new_data):
 		if prev_node == None:
 			return
 
@@ -26,88 +28,152 @@ class LinkedList:
 		prev_node.next = new_node
 
 
-	def append(self, new_data):   
-		# 1. Create a new node 
-		# 2. Put in the data 
-		# 3. Set next as None 
+	def append(self, new_data):
+		# 1. Create a new node
+		# 2. Put in the data
+		# 3. Set next as None
 
 		new_node = Node(new_data)
-		# 4. If the Linked List is empty, then make the 
+		# 4. If the Linked List is empty, then make the
 		#    new node as head
-		if self.head is None: 
-			self.head = new_node 
+		if self.head is None:
+			self.head = new_node
 			return
 
-		# 5. Else traverse till the last node 
-		last = self.head 
-		while (last.next): 
+		# 5. Else traverse till the last node
+		last = self.head
+		while (last.next):
 			last = last.next
 
-		# 6. Change the next of last node 
+		# 6. Change the next of last node
 		last.next =  new_node
 
 
-	# Given a reference to the head of a list and a key,  
-	# delete the first occurence of key in linked list  
-	def deleteNode(self, key):  
-		  
-		# Store head node  
-		temp = self.head  
-  
-		# If head node itself holds the key to be deleted  
-		if (temp is not None):  
-			if (temp.data == key):  
+	# Given a reference to the head of a list and a key,
+	# delete the first occurence of key in linked list
+	def deleteNode(self, key):
+
+		# Store head node
+		temp = self.head
+
+		# If head node itself holds the key to be deleted
+		if (temp is not None):
+			if (temp.data == key):
 				self.head = temp.next
 				temp = None
 				return
-  
-		# Search for the key to be deleted, keep track of the  
-		# previous node as we need to change 'prev.next'  
-		while(temp is not None):  
-			if temp.data == key:  
+
+		# Search for the key to be deleted, keep track of the
+		# previous node as we need to change 'prev.next'
+		while(temp is not None):
+			if temp.data == key:
 				break
-			prev = temp  
+			prev = temp
 			temp = temp.next
-  
-		# if key was not present in linked list  
-		if(temp == None):  
+
+		# if key was not present in linked list
+		if(temp == None):
 			return
-  
-		# Unlink the node from linked list  
+
+		# Unlink the node from linked list
 		prev.next = temp.next
-  
+
 		temp = None
 
+	def deleteMiddle(self):
+		if self.head.next is None:
+			self.head = None
+			return
+		slow, fast = self.head, self.head
+		prev_slow = None
 
-		# Utility function to print the linked list 
-	def printList(self): 
-		temp = self.head 
-		while (temp): 
-			print(temp.data) 
+		while fast and fast.next:
+			prev_slow = slow
+			slow = slow.next
+			fast = fast.next.next
+
+		prev_slow.next = slow.next
+		slow = None
+		return self.head
+
+
+	def oddEvenList(self):
+		if not self.head:
+			return
+
+		count = 1
+		even = None
+		odd = None
+		evenHead = None
+		startNode = self.head
+
+		while startNode:
+			# prev_node = startNode
+			if count%2 != 0:
+				if odd is None:
+					odd = startNode
+				else:
+					odd.next = startNode
+					odd = odd.next
+			else:
+				if even is None:
+					even = startNode
+					evenHead = startNode
+				else:
+					even.next = startNode
+					even = even.next
+			startNode = startNode.next
+			count += 1
+		odd.next= evenHead
+		if even:
+			even.next = None
+		return self.head
+
+
+	def reverseList(self):
+		if not self.head:
+			return
+
+		curr = self.head
+		prev = None
+
+		while curr:
+			nextNode = curr.next
+			curr.next = prev
+			prev = curr
+			curr = nextNode
+		self.head = prev
+
+
+	# Utility function to print the linked list
+	def printList(self):
+		temp = self.head
+		while (temp):
+			print(temp.data)
 			temp = temp.next
-  
-  
-  
-# Code execution starts here 
-if __name__=='__main__': 
-  
-	# Start with the empty list 
-	llist = LinkedList() 
-  
-	# Insert 6.  So linked list becomes 6->None 
-	llist.append(6) 
-  
-	# Insert 7 at the beginning. So linked list becomes 7->6->None 
+
+
+# Code execution starts here
+if __name__=='__main__':
+	# Start with the empty list
+	llist = LinkedList()
+	# Insert 6.  So linked list becomes 6->None
+	llist.append(6)
+	# Insert 7 at the beginning. So linked list becomes 7->6->None
 	llist.insertAtFirst(7)
-  
-	# Insert 1 at the beginning. So linked list becomes 1->7->6->None 
+	# Insert 1 at the beginning. So linked list becomes 1->7->6->None
 	llist.insertAtFirst(1)
-  
-	# Insert 4 at the end. So linked list becomes 1->7->6->4->None 
-	llist.append(4) 
-  
-	# Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None 
-	llist.insertAfter(llist.head.next, 8) 
-  
+	# Insert 4 at the end. So linked list becomes 1->7->6->4->None
+	llist.append(4)
+	# Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None
+	llist.insertAfter(llist.head.next, 8)
+	# llist.deleteMiddle()
 	print('Created linked list is:')
-	llist.printList() 
+	llist.printList()
+	# llist.oddEvenList()
+	# print('Created linked list is:')
+	# llist.printList()
+	llist.reverseList()
+	print('Created linked list is:')
+	llist.printList()
+
